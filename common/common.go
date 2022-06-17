961e30bde2eef8cd0e7c0bfc738d49519a0e3b95
your_username <yarox@dontreply.com>
2022-06-17 18:25:13 +0200

    Remove hardcoded dir

--- common/common.go
+++ common/common.go
@@ -20,7 +20,7 @@ func Determine_Maps_Path(MapsDirectoryConfig string) (string, error) {
 
 	maps_dir := ""
 
-	MapsDirectoryConfig = "F:\\GoLangBase\\EvtxHussarProject\\maps"
+	//MapsDirectoryConfig = "F:\\GoLangBase\\EvtxHussarProject\\maps"
 
 	// Auto-detection based on binary
 	if MapsDirectoryConfig == "" {
